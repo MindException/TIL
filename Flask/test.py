279334df75dc4547ecb6a7171d35071b717916d3
MindException <tourer97@naver.com>
2023-02-08 10:32:25 +0900

    01.flask 추가

---
+++ Flask/test.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def test1():                           
+    return "Server Open!!"
+
+@app.route("/hello/<username>")
+def test2():                           
+    return "Hello Flask!"
+
+if __name__ == "__main__":              
+    app.run(host="127.0.0.1", port="8123")
