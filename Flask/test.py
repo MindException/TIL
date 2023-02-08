b8dab2e6663c454547083b8d94fe4b15de45e72e
MindException <tourer97@naver.com>
2023-02-08 13:51:41 +0900

    Rest Api-01 추가

--- Flask/test.py
+++ Flask/test.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -7,8 +7,13 @@ def test1():
     return "Server Open!!"
 
 @app.route("/hello/<username>")
-def test2():                           
+def test2(username):                           
     return "Hello Flask!"
+
+@app.route("/jsontest")
+def jsontest():
+    data = {'file' : 'jsontest', 'text' : 'hi! jsontest!'}
+    return jsonify(data)
 
 if __name__ == "__main__":              
     app.run(host="127.0.0.1", port="8123")
